575c3271c9829114dadfd1950ee2e769670edf2e
Viktor S. Romild <viktor.sandstrom@animech.com>
2021-06-15 17:05:27 +0200

    feat: get summary from wikipedia via the terminal.

---
+++ mod.ts
@@ -0,0 +1,15 @@
+const _permission = await Deno.permissions.request({name: "net"});
+
+const term = Deno.args.join(' ');
+
+const fetchUrl = `https://en.wikipedia.org/api/rest_v1/page/summary/${term}?redirect=true`;
+
+const result = await fetch(fetchUrl);
+
+const json = await result.json();
+
+if (json.extract) {
+  console.log(json.extract);
+} else {
+  console.log(`Could not fetch wiki for your input: ${term}`);
+}
