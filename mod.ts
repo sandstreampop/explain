e906beafc8a77db9ea8ad7fe26c14fdf1d5d0e61
Viktor S. Romild <viktor.sandstrom@animech.com>
2021-06-15 22:31:47 +0200

    refactor: break out smaller function, early exit

--- mod.ts
+++ mod.ts
@@ -13,31 +13,44 @@ async function explain(term: string) {
   );
   const fetchResult = await fetch(url);
   const json: WikiFetchResult = await fetchResult.json();
-  
+
   const { extract } = json;
+
   if (extract === undefined) {
     console.log(`Could not fetch wiki for your input: ${term}`);
-  } else {
-    if (extract.includes("may refer to:")) {
-      const htmlUrl = encodeURI(
-        `https://en.wikipedia.org/api/rest_v1/page/html/${term}?redirect=true&stash=false`
-      );
-      const htmlResult = await fetch(htmlUrl);
-      const htmlString = await htmlResult.text();
-      const parsedHtml = new DOMParser().parseFromString(htmlString, "text/html");
-      const disambiguationList = parsedHtml
-        ?.getElementsByTagName("a")
-        .filter((el) => el.getAttribute("rel") === "mw:WikiLink")
-        .map((el) => el.textContent);
-      console.table(disambiguationList);
-      console.log("There is more than one match. Please input the index of the term you meant:");
-      const data = await readLines(Deno.stdin).next();
-      console.log(disambiguationList![data.value]);
-      explain(disambiguationList![data.value]);
-    } else {
-      console.log(extract);
-    }
+    Deno.exit();
   }
+
+  if (!extract.includes("may refer to:")) {
+    console.log(extract);
+    Deno.exit();
+  }
+
+  handleDisambiguation(term);
+}
+
+async function handleDisambiguation(term: string) {
+  const htmlUrl = encodeURI(
+    `https://en.wikipedia.org/api/rest_v1/page/html/${term}?redirect=true&stash=false`
+  );
+  const htmlResult = await fetch(htmlUrl);
+  const htmlString = await htmlResult.text();
+  
+  const parsedHtml = new DOMParser().parseFromString(htmlString, "text/html");
+  const disambiguationList = parsedHtml
+    ?.getElementsByTagName("a")
+    .filter((el) => el.getAttribute("rel") === "mw:WikiLink")
+    .map((el) => el.textContent);
+  
+  console.table(disambiguationList);
+  console.log(
+    "There is more than one match. Please input the index of the term you meant:"
+  );
+  
+  const data = await readLines(Deno.stdin).next();
+  
+  console.log(disambiguationList![data.value]);
+  explain(disambiguationList![data.value]);
 }
 
 const termFromArgs = Deno.args.join(" ");
