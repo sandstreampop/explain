113ea35e37202f006ef6206de59575877d973b38
Viktor Sandström Romild <viktorsandstrom@gmail.com>
2022-11-22 09:54:11 +0100

    update dependencies

--- mod.ts
+++ mod.ts
@@ -1,5 +1,5 @@
-import { DOMParser } from "https://deno.land/x/deno_dom@v0.1.12-alpha/deno-dom-wasm.ts";
-import { readLines } from "https://deno.land/std@0.98.0/io/mod.ts";
+import { DOMParser } from "https://deno.land/x/deno_dom@v0.1.36-alpha/deno-dom-wasm.ts";
+import { readLines } from "https://deno.land/std@0.165.0/io/mod.ts";
 
 interface WikiFetchResult {
   extract?: string;
@@ -9,7 +9,7 @@ const _permission = await Deno.permissions.request({ name: "net" });
 
 async function explain(term: string) {
   const url = encodeURI(
-    `https://en.wikipedia.org/api/rest_v1/page/summary/${term}?redirect=true`
+    `https://en.wikipedia.org/api/rest_v1/page/summary/${term}?redirect=true`,
   );
   const fetchResult = await fetch(url);
   const json: WikiFetchResult = await fetchResult.json();
@@ -31,24 +31,24 @@ async function explain(term: string) {
 
 async function handleDisambiguation(term: string) {
   const htmlUrl = encodeURI(
-    `https://en.wikipedia.org/api/rest_v1/page/html/${term}?redirect=true&stash=false`
+    `https://en.wikipedia.org/api/rest_v1/page/html/${term}?redirect=true&stash=false`,
   );
   const htmlResult = await fetch(htmlUrl);
   const htmlString = await htmlResult.text();
-  
+
   const parsedHtml = new DOMParser().parseFromString(htmlString, "text/html");
   const disambiguationList = parsedHtml
     ?.getElementsByTagName("a")
     .filter((el) => el.getAttribute("rel") === "mw:WikiLink")
     .map((el) => el.textContent);
-  
+
   console.table(disambiguationList);
   console.log(
-    "There is more than one match. Please input the index of the term you meant:"
+    "There is more than one match. Please input the index of the term you meant:",
   );
-  
+
   const data = await readLines(Deno.stdin).next();
-  
+
   console.log(disambiguationList![data.value]);
   explain(disambiguationList![data.value]);
 }
